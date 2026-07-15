7e71602b231caf5163474514dec30e6215108a02
yutao <642231346@qq.com>
2026-07-15 19:48:03 +0800

    修改进度条对应积分

--- assets/script/const/PingPangProgressConfig.ts
+++ assets/script/const/PingPangProgressConfig.ts
@@ -4,22 +4,22 @@ export interface IPingPangProgressMilestone {
   label: string;
 }
 
-export const PingPangProgressMaxScore = 500;
+export const PingPangProgressMaxScore = 300;
 
 export const PingPangProgressMilestones: IPingPangProgressMilestone[] = [
   {
     key: 'point100',
     score: 50,
-    label: '100',
+    label: '50',
   },
   {
     key: 'point300',
-    score: 300,
-    label: '300',
+    score: 100,
+    label: '100',
   },
   {
     key: 'point500',
-    score: 500,
-    label: '500',
+    score: 300,
+    label: '300',
   },
 ];
